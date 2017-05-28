44bb57320153de21381a7088f74c51fff5444b41
sebmichalowski <seb.michalowski@gmail.com>
2017-05-29 01:43:14 +0200

    EventCategoryDaoSqlite find() method implemented

--- src/main/java/pl/websm/dao/EventCategoryDaoSqlite.java
+++ src/main/java/pl/websm/dao/EventCategoryDaoSqlite.java
@@ -1,4 +1,42 @@
 package pl.websm.dao;
 
-public class EventCategoryDaoSqlite {
+import pl.websm.model.EventCategory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class EventCategoryDaoSqlite implements EventCategoryDao{
+
+    private Connection connection;
+    private PreparedStatement statement;
+    private ResultSet resultSet;
+
+    @Override
+    public EventCategory find(Integer id) {
+        EventCategory eventCategory = null;
+
+        try {
+            connection = SQLiteJDBCConnector.connection();
+            statement = connection.prepareStatement("SELECT * FROM categories WHERE id=(?)");
+            statement.setInt(1, id);
+            resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                eventCategory = new EventCategory(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return eventCategory;
+    }
+
+    @Override
+    public List<EventCategory> getAll() {
+        return null;
+    }
 }
