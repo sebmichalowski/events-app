3311ae0fe8edc8a1cbeb62ec068b73ca3a9b3fce
sebmichalowski <seb.michalowski@gmail.com>
2017-05-29 00:59:33 +0200

    EventCategory class implemented in Model package

--- src/main/java/pl/websm/model/EventCategory.java
+++ src/main/java/pl/websm/model/EventCategory.java
@@ -1,4 +1,12 @@
 package pl.websm.model;
 
-public class EventCategory {
+public class EventCategory extends BaseModel {
+
+    public EventCategory(Integer id, String name) {
+        super(id, name);
+    }
+
+    public EventCategory(String name) {
+        super(name);
+    }
 }
