2f996b32a522b96d60f236733764ff9ad36a7d6f
sebmichalowski <seb.michalowski@gmail.com>
2017-05-29 00:58:56 +0200

    Event class implemented in Model package

--- src/main/java/pl/websm/model/Event.java
+++ src/main/java/pl/websm/model/Event.java
@@ -1,4 +1,57 @@
 package pl.websm.model;
 
-public class Event {
+public class Event extends BaseModel {
+
+    private String description;
+    private String date;
+    private EventCategory eventCategory;
+    private String link;
+
+    public Event(Integer id, String name, String description, String date, EventCategory eventCategory, String link){
+        super(id, name);
+        setDescription(description);
+        setDate(date);
+        setEventCategory(eventCategory);
+        setLink(link);
+    }
+
+    public Event(String name, String description, String date, EventCategory eventCategory, String link){
+        super(name);
+        setDescription(description);
+        setDate(date);
+        setEventCategory(eventCategory);
+        setLink(link);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    private void setDate(String date) {
+        this.date = date;
+    }
+
+    public EventCategory getEventCategory() {
+        return eventCategory;
+    }
+
+    private void setEventCategory(EventCategory eventCategory) {
+        this.eventCategory = eventCategory;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    private void setLink(String link) {
+        this.link = link;
+    }
 }
