a4aa74ac86002de76c71c58df18f521465d9e997
sebmichalowski <seb.michalowski@gmail.com>
2017-05-29 01:59:07 +0200

    EventDaoSqlite save() method implemented

--- src/main/java/pl/websm/dao/EventDaoSqlite.java
+++ src/main/java/pl/websm/dao/EventDaoSqlite.java
@@ -1,4 +1,70 @@
 package pl.websm.dao;
 
-public class EventDaoSqlite {
+import pl.websm.model.Event;
+import pl.websm.model.EventCategory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class EventDaoSqlite implements EventDao{
+
+    private Connection connection;
+    private PreparedStatement statement;
+
+    @Override
+    public void save(Event event) {
+        try{
+            connection = SQLiteJDBCConnector.connection();
+
+            if (event.getId() != null){
+                statement = connection.prepareStatement(
+                        "UPDATE events SET " +
+                                "name=(?), " +
+                                "date=(?), " +
+                                "description=(?), " +
+                                "category_id=(?), " +
+                                "link=(?) " +
+                                "WHERE id =(?);");
+                statement.setInt(6, event.getId());
+            } else {
+                statement = connection.prepareStatement(
+                        "INSERT INTO events " +
+                                "(name, date, description,  category_id, link)" +
+                                "VALUES ((?), (?), (?), (?), (?))");
+            }
+            statement.setString(1, event.getName());
+            statement.setString(2, event.getDate());
+            statement.setString(3, event.getDescription());
+            statement.setInt(4, event.getEventCategory().getId());
+            statement.setString(5, event.getLink());
+            statement.execute();
+            statement.close();
+            connection.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Event find(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void remove(Integer id) {
+
+    }
+
+    @Override
+    public List<Event> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Event> getBy(EventCategory eventCategory) {
+        return null;
+    }
 }
