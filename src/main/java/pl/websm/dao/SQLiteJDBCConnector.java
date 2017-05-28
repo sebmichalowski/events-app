f8da6e449a97f8fe47689042d82327b27de1a2b1
sebmichalowski <seb.michalowski@gmail.com>
2017-05-29 01:09:52 +0200

    SQLiteJDBCConnector createTables() method implemented

--- src/main/java/pl/websm/dao/SQLiteJDBCConnector.java
+++ src/main/java/pl/websm/dao/SQLiteJDBCConnector.java
@@ -3,8 +3,12 @@ package pl.websm.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SQLiteJDBCConnector {
+
+    private static Connection connection;
+    private static Statement statement;
 
     static Connection connection() {
         Connection connection = null;
@@ -16,5 +20,27 @@ public class SQLiteJDBCConnector {
             System.exit(1);
         }
         return connection;
+    }
+
+    private static void createTables()throws SQLException {
+        connection = connection();
+        statement = connection.createStatement();
+        statement.execute(
+                "CREATE TABLE IF NOT EXISTS events \n" +
+                        "(" +
+                        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        "name TEXT," +
+                        "date TEXT," +
+                        "description TEXT," +
+                        "category_id INTEGER," +
+                        "link TEXT" +
+                        ")");
+        statement.execute(
+                "CREATE TABLE IF NOT EXISTS categories" +
+                        "(" +
+                        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        "name TEXT\n" +
+                        ")");
+        System.out.println("Database tables created.");
     }
 }
