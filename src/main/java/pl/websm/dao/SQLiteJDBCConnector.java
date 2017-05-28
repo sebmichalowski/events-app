1385eb1d61372901fe50c9ba2e25f3bce4b3be40
sebmichalowski <seb.michalowski@gmail.com>
2017-05-29 01:06:36 +0200

    SQLiteJDBCConnector connection() method implemented

--- src/main/java/pl/websm/dao/SQLiteJDBCConnector.java
+++ src/main/java/pl/websm/dao/SQLiteJDBCConnector.java
@@ -1,4 +1,20 @@
 package pl.websm.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class SQLiteJDBCConnector {
+
+    static Connection connection() {
+        Connection connection = null;
+        try {
+            String url = "jdbc:sqlite:src/main/resources/database.db";
+            connection = DriverManager.getConnection(url); // create a connection to the database
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+        return connection;
+    }
 }
