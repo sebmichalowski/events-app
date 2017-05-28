624678544c713b68b275ff90c7d0dca676a3c65d
sebmichalowski <seb.michalowski@gmail.com>
2017-05-29 01:23:19 +0200

    EventCategoryDao interface implemented

--- src/main/java/pl/websm/dao/EventCategoryDao.java
+++ src/main/java/pl/websm/dao/EventCategoryDao.java
@@ -1,4 +1,12 @@
 package pl.websm.dao;
 
+import pl.websm.model.EventCategory;
+
+import java.util.List;
+
 public interface EventCategoryDao {
+
+    EventCategory find(Integer id);
+
+    List<EventCategory> getAll();
 }
